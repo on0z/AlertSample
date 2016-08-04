b24d1afe10b0819b7ff4987f7baca497c4573fd1
原園征志 <r15113@osaka-pct.ac.jp>
2016-08-04 13:08:06 +0900

    targetを8.0に変更 swift3.0で書き換え(Xcode8 beta4)

--- AlertSample/ViewController.swift
+++ AlertSample/ViewController.swift
@@ -2,32 +2,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func myButton(sender: AnyObject) {
+    @IBAction func myButton(_ sender: AnyObject) {
         Alert()
     }
-    @IBAction func ActionSheet(sender: AnyObject) {
+    @IBAction func ActionSheet(_ sender: AnyObject) {
         ActionSheet()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.whiteColor()
+        self.view.backgroundColor = UIColor.white
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     func Alert(){
-        let myAlert: UIAlertController = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .Alert)
-        let OKAkction: UIAlertAction = UIAlertAction(title: "OK", style: .Cancel, handler: {UIAlertAction in print(myAlert.textFields![0].text ?? "nil")})
-        myAlert.addTextFieldWithConfigurationHandler(nil)
+        let myAlert: UIAlertController = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .alert)
+        let OKAkction: UIAlertAction = UIAlertAction(title: "OK", style: .cancel, handler: {UIAlertAction in print(myAlert.textFields![0].text ?? "nil")})
+        myAlert.addTextField(configurationHandler: nil)
         myAlert.addAction(OKAkction)
-        self.presentViewController(myAlert, animated: true, completion: nil)
+        self.present(myAlert, animated: true, completion: nil)
     }
     
     func ActionSheet(){
-        let myActionSheet: UIAlertController = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .ActionSheet)
-        let OKAkction: UIAlertAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+        let myActionSheet: UIAlertController = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .actionSheet)
+        let OKAkction: UIAlertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
         myActionSheet.addAction(OKAkction)
-        self.presentViewController(myActionSheet, animated: true, completion: nil)
+        self.present(myActionSheet, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
